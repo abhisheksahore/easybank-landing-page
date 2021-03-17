c06b3c2f240a1d53b8f0d69c32bfacfe0ccb3222
abhisheksahore <abhisheksahore7@gmail.com>
2021-03-17 23:57:39 +0530

    fixed the mockup screens on toggle the hamburger menu its display becomes none.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 const hamburger_toggler = document.querySelector('.hamburger');
 const nav_menu = document.querySelector('.nav-menu')
 const close_nav_menu = document.querySelector('.close-nav-menu');
+const intro_mockup = document.querySelector('.intro-mockup-img');
+
 
 hamburger_toggler.addEventListener('click', function() {
+    intro_mockup.setAttribute('id', 'intro-mockup-img-remove');
     nav_menu.classList.toggle('nav-menu-open');
     hamburger_toggler.classList.toggle('hamburger-open');
     close_nav_menu.classList.toggle('open-nav-menu');
@@ -11,6 +14,7 @@ hamburger_toggler.addEventListener('click', function() {
 
 close_nav_menu.addEventListener('click', function() {
     nav_menu.classList.toggle('nav-menu-open');
+    intro_mockup.removeAttribute('id');
     hamburger_toggler.classList.toggle('hamburger-open');
     close_nav_menu.classList.toggle('open-nav-menu');
 })
